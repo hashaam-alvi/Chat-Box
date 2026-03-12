13e38f3a928f3c773054da2c38ff2b096315feeb
Hashaam Alvi <hashaam.imran.alvi@gmail.com>
2026-03-12 16:26:40 +0500

    Server is deployed to railway

--- Server/index.js
+++ Server/index.js
@@ -9,13 +9,25 @@ app.use(cors());
 app.use(express.json()); 
 
 // const io = require('socket.io')(server);
-const io = new Server(server, {
+/* const io = new Server(server, {
   cors: {
     // origin: "*",
     origin: "https://chat-box-production-ecb4.up.railway.app",
+    //  origin: "https://chat-box-lime-pi.vercel.app",
+    //  methods: ["GET", "POST"],
   },
-});
+}); */
 
+
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:5173",
+      "https://chat-box-lime-pi.vercel.app"
+    ]
+  },
+  transports: ["websocket","polling"]
+});
 
 io.on('connection', (socket) => { 
     console.log("User connected:", socket.id);
@@ -32,11 +44,11 @@ io.on('connection', (socket) => {
   });
  });
 
-// const PORT = process.env.PORT || 5000;
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT;
 
 server.listen(PORT, () => {
-  console.log("Server running on port 5000");
+  console.log(`Server running on port ${PORT}`);
 });
 
 
